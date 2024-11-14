89c016d97db790345edb670ec854cf393bcdc7ae
SofyaMerkulova <merk3010@gmail.com>
2024-11-14 18:48:52 +0300

    Добавьте файлы проекта.

---
+++ Tasks_from_file2/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Net.Security;
+using System.Runtime.InteropServices;
+namespace Tasks_from_file2
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1. Выведите на экран информацию о каждом типе данных в виде:
+            Тип данных – максимальное значение – минимальное значение*/
+            Console.WriteLine("Задание 1");
+            byte byteMin = byte.MinValue;
+            byte byteMax = byte.MaxValue;
+            Console.WriteLine($"Тип данных - byte - максимальное значение - {byteMax} - минимальное значение - {byteMin}");
+            sbyte sbyteMin = sbyte.MinValue;
+            sbyte sbyteMax = sbyte.MaxValue;
+            Console.WriteLine($"Тип данных - sbyte - максимальное значение - {sbyteMax} - минимальное значение - {sbyteMin}");
+            short shortMin = short.MinValue;
+            short shortMax = short.MaxValue;
+            Console.WriteLine($"Тип данных - short - максимальное значение - {sbyteMax} - минимальное значение - {sbyteMin}");
+            ushort ushortMin = ushort.MinValue;
+            ushort ushortMax = ushort.MaxValue;
+            Console.WriteLine($"Тип данных - ushort - максимальное значение - {ushortMax} - минимальное значение - {ushortMin}");
+            int intMin = int.MinValue;
+            int intMax = int.MaxValue;
+            Console.WriteLine($"Тип данных - int - максимальное значение - {intMax} - минимальное значение - {intMin}");
+            uint uintMin = uint.MinValue;
+            uint uintMax = uint.MaxValue;
+            Console.WriteLine($"Тип данных - uint - максимальное значение - {uintMin} - минимальнре значение - {uintMax}");
+            ulong ulongMin = ulong.MinValue;
+            ulong ulongMax = ulong.MaxValue;
+            Console.WriteLine($"Тип данных - ulong - максимальное значение - {ulongMax} - минимальное значение - {ulongMin}");
+            float floatMin = float.MinValue;
+            float floatMax = float.MaxValue;
+            Console.WriteLine($"Тип данных - float - максимальное значение - {floatMax} - минимальное значение - {floatMin}");
+            double doubleMin = double.MinValue;
+            double doubleMax = double.MaxValue;
+            Console.WriteLine($"Тип данных - double - максимальное значение - {doubleMax} - минимальное значение - {doubleMin}");
+            char charMin = char.MinValue;
+            char charMax = char.MaxValue;
+            Console.WriteLine($"Тип данных - char - максимальное значение - {charMax} - минимальное значение - {charMin}");
+            decimal decimalMin = decimal.MinValue;
+            decimal decimalMax = decimal.MaxValue;
+            Console.WriteLine($"Тип данных - decimal - максимальное значение - {decimalMax} - минимальное значение  - {decimalMin}");
+            Console.WriteLine(" ");
+            /*2. Напишите программу, в которой принимаются данные пользователя в виде имени,
+            города, возраста и PIN-кода. Далее сохраните все значение в соответствующей
+            переменной, а затем распечатайте всю информацию в правильном формате.*/
+            Console.WriteLine("Задание 2");
+            Console.Write("Введите Ваше имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите Ваш город: ");
+            string town = Console.ReadLine();
+            Console.Write("Введите Ваш возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите Ваш PIN-код: ");
+            int pin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Вас зовут: {name} , Вам: {age} года/лет, Вы живёте в городе: {town} , Ваш PIN-код: {pin} ");
+            Console.WriteLine(" ");
+            /*3. Преобразуйте входную строку: строчные буквы замените на заглавные, заглавные – на
+            строчные.*/
+            Console.WriteLine("Задание 3");
+            string sentence;
+            string then;
+            Console.WriteLine("Введите строку, используя заглавные и строчные буквы: ");
+            sentence = Console.ReadLine();
+            then = "";
+            foreach (char word in sentence)
+            {
+                if (char.IsLower(word))
+                    then += char.ToUpper(word);
+                if (char.IsUpper(word))
+                    then += char.ToLower(word);
+            }
+            Console.WriteLine($"{then}");
+            /*4. Введите строку, введите подстроку. Необходимо найти количество вхождений и вывести
+            на экран.*/
+            Console.WriteLine("Задание 4");
+            string question = "How are you doing and where are you now";
+            string substring = "you";
+            int result = question.IndexOf(substring);
+            Console.WriteLine(result);
+            Console.WriteLine(" ");
+            /*5. Цель этой задачи - определить, сколько бутылок виски беспошлинной торговли вам
+          нужно будет купить, чтобы экономия по сравнению с обычной средней ценой фактически
+          покрыла расходы на ваш отпуск. Вам будет предоставлена стандартная цена (normPrice),
+          скидка в Duty Free (salePrice) и стоимость отпуска (holidayPrice). Например, если бутылка
+          обычно стоит 10 фунтов стерлингов, а скидка в дьюти фри составляет 10%, вы
+          сэкономите 1 фунт стерлингов на каждой бутылке. Если ваш отпуск стоит 500 фунтов
+          стерлингов, ответ, который вы должны вернуть, будет 500. Все входные данные будут
+          целыми числами. Пожалуйста, верните целое число. Округлить в меньшую сторону.*/
+            Console.WriteLine("Задание 5");
+            int normPrice;
+            Console.Write("Введите стандартную стоимость (целое число): ");
+            normPrice = Convert.ToInt32(Console.ReadLine());
+            int salePrice;
+            Console.Write("Введите  скидку в Duty Free (целое число без процентного знака): ");
+            salePrice = Convert.ToInt32(Console.ReadLine());
+            int holidayPrice;
+            Console.Write("Введите стоимость отпуска (целое число): ");
+            holidayPrice = Convert.ToInt32(Console.ReadLine());
+            int answer = holidayPrice / (normPrice * salePrice / 100);
+            double answer2 = answer;
+            double answer3 = Math.Floor(answer2);
+            Console.WriteLine($"Количество бутылок равняется: {answer3}");
+        
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+}
+
---
+++ Tumakov_lab3/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Tumakov_lab3
+{
+    enum bankAccounts
+    {
+        Текущий,
+        Сберегательный
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Упражнение 3.1 Создать перечислимый тип данных отображающий
+           виды банковского счета (текущий и сберегательный). Создать переменную типа
+           перечисления, присвоить ей значение и вывести это значение на печать.*/
+            Console.WriteLine("Упражнение 3.1");
+            bankAccounts yourAccountType = bankAccounts.Текущий;
+            Console.WriteLine("Вид банковского счёта - {0}", yourAccountType, (int)yourAccountType);
+            Console.WriteLine(" ");
+            /*Упражнение 3.2 Создать структуру данных, которая хранит информацию
+            о банковском счете – его номер, тип и баланс. Создать переменную такого типа,
+            заполнить структуру значениями и напечатать результат.*/
+            Console.WriteLine("Упражнение 3.2");
+            BankAccount account = new BankAccount();
+            account.number = 56927;
+            account.type = "Сберегательный счёт";
+            account.balance = 4500000;
+            account.Print();
+            SecondMain();
+        }
+        /// <summary>
+        /// Структура данных, хранящая информацию о банковском счёте
+        /// </summary>
+        struct BankAccount
+        {
+            /// <summary>
+            /// Выводит информацию о номере банковского счёта
+            /// </summary>
+            public int number;
+            /// <summary>
+            /// Выводит информацию о типе банковкого счёта
+            /// </summary>
+            public string type;
+            /// <summary>
+            /// Выводит информацию о балнсе банковского счёта
+            /// </summary>
+            public int balance;
+            public void Print()
+            {
+
+
+                Console.WriteLine($"Номер счёта:{number}, тип счёта: {type} , баланс: {balance}");
+                Console.WriteLine(" ");
+                Console.WriteLine("Домашнее задание 3.1");
+            }
+        }
+        /*Домашнее задание 3.1 Создать перечислимый тип ВУЗ {КГУ, КАИ,
+        КХТИ}. Создать структуру работник с двумя полями: имя, ВУЗ. Заполнить
+        структуру данными и распечатать.*/
+        enum ВУЗ
+        {
+            КГУ,
+            КАИ,
+            КХТИ
+        }
+            static void SecondMain()
+            {
+                Worker worker = new Worker();
+                worker.workerName1 = "Иванов Александр Павлович";
+                worker.university1 = "КГУ";
+                worker.workerName2 = "Королёва Ольга Юрьевна";
+                worker.university2 = "КАИ";
+                worker.workerName3 = "Зуева Луиза Амировна";
+                worker.university3 = "КХТИ";
+                worker.Print();
+
+            }
+        /// <summary>
+        /// Структура данных, хранящая информацию о работниках ВУЗов
+        /// </summary>
+            struct Worker
+            {
+            /// <summary>
+            /// Выводит информацию об имени первого работника
+            /// </summary>
+                public string workerName1;
+            /// <summary>
+            /// Выводит информацию об имени второго работника
+            /// </summary>
+                public string workerName2;
+            /// <summary>
+            /// Выводит информацию об имени третьего работника
+            /// </summary>
+                public string workerName3;
+            /// <summary>
+            /// Выводит название первого ВУЗа
+            /// </summary>
+                public string university1;
+            /// <summary>
+            /// ВЫводит название второго ВУЗа
+            /// </summary>
+                public string university2;
+            /// <summary>
+            /// Выводит название третьего ВУЗа
+            /// </summary>
+                public string university3;
+                public void Print()
+                {
+                    Console.WriteLine($"Имя работника:{workerName1},  ВУЗ: {university1}");
+                    Console.WriteLine($"Имя работника:{workerName2},  ВУЗ: {university2}");
+                    Console.WriteLine($"Имя работника: {workerName3},  ВУЗ: {university3}");
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+
+
